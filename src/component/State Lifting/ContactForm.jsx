28c3d1f32de8ad583f36852c2bc895e632e1a991
Mosaddik Billah <mosaddik.b1307@gmail.com>
2024-11-26 06:30:24 +0600

    state lifting project done BUT...
    -ekhane filter ebong search kaj kore na. eta thik korte hobe

--- src/component/State Lifting/ContactForm.jsx
+++ src/component/State Lifting/ContactForm.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 const CONTACT_FORM_INIT_STATE = {
   name: "",
   email: "",
+  group: "",
 };
 
 const ContactForm = ({ getContact }) => {
   const [values, setValues] = useState({ ...CONTACT_FORM_INIT_STATE });
-  const { name, email } = values;
+  const { name, email, group } = values;
 
   const handleChange = (e) => {
     setValues({
@@ -30,6 +31,14 @@ const ContactForm = ({ getContact }) => {
         <div>
           <label htmlFor="email">Email:</label>
           <input type="text" id="email" name="email" value={email} onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="group">Group</label>
+          <select name="group" id="group" onChange={handleChange} value={group}>
+            <option value="">Select Group</option>
+            <option value="Home">Home</option>
+            <option value="Office">Office</option>
+          </select>
         </div>
         <input
           type="submit"
--- src/component/State Lifting/Table.jsx
+++ src/component/State Lifting/Table.jsx
@@ -1,6 +1,54 @@
+/**
+ * bug:(filter) ekhankar filter er kaj valo moto bujhi nai. filter kaj kore na. eta thik korte hobe.
+ * bug: (search) search kaj kore nai. eta thiik kora lagbe
+ */
+import { useState } from "react";
+
 const Table = ({ contacts }) => {
+  const [filter, setFilter] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+    console.log(e.target.value);
+  };
+
+  let filteredContact = [];
+
+  if (filter === "All") {
+    filteredContact = contacts;
+  } else {
+    filteredContact = contacts.filter((contact) => {
+      contact.group.toString() === filter;
+    });
+  }
+
+  if (searchTerm) {
+    filteredContact = filteredContact.filter(
+      (contact) => contact.name.includes(searchTerm) || contact.email.includes(searchTerm)
+    );
+    console.log(searchTerm);
+  }
+
   return (
     <div style={{ margin: "20px" }}>
+      <div>
+        Filter
+        <select value={filter} onChange={handleChange}>
+          <option value="All">All</option>
+          <option value="">None</option>
+          <option value="Home">Home</option>
+          <option value="Office">Office</option>
+        </select>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <table>
         <thead>
           <tr>
@@ -8,6 +56,7 @@ const Table = ({ contacts }) => {
               <>
                 <th>Name</th>
                 <th>Email</th>
+                <th>Group</th>
               </>
             ) : (
               <th>Data Not Found</th>
@@ -15,10 +64,11 @@ const Table = ({ contacts }) => {
           </tr>
         </thead>
         <tbody>
-          {contacts.map((contact, index) => (
+          {filteredContact.map((contact, index) => (
             <tr key={index}>
               <td>{contact.name}</td>
               <td>{contact.email}</td>
+              <td>{contact.group}</td>
             </tr>
           ))}
         </tbody>
