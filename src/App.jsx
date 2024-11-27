622fe06b99647982fffe823e28b076d468d6bd26
Mosaddik Billah <mosaddik.b1307@gmail.com>
2024-11-27 18:51:55 +0600

    Fetch and UseEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ library.add(fas, fab);
 import "./assets/css/style.css";
 import "milligram";
 //module import file
-import StateLifting from "./component/State Lifting/StateLifting";
+import SingleUser from "./component/Fetch/Singleuser";
 const App = () => {
   return (
     <div>
-      <StateLifting />
+      <SingleUser />
     </div>
   );
 };
