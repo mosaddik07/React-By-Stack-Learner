2a332d1cceb3000e786c55d8c600a456b25bee31
Mosaddik Billah <mosaddik.b1307@gmail.com>
2024-11-30 11:31:12 +0600

    Custom Hooks Done

--- src/component/hooks/useFetch.js
+++ src/component/hooks/useFetch.js
@@ -1,4 +1,33 @@
-/**
- * todo:
- */
-const useFetch = () => {};
+import { useState, useEffect } from "react";
+
+const useFetch = (url, cb) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  // setLoading(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const result = await res.json();
+      if (cb) {
+        setData(cb(result));
+      } else {
+        setData(result);
+      }
+      setError("");
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error };
+};
+export default useFetch;
